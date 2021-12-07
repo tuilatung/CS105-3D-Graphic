16f872191254c1fc57aecea230b6a9368573b504
Tung Huynh <19522492@gm.uit.edu.vn>
2021-12-07 16:11:04 +0700

    setup the renderer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { TeapotGeometry } from 'three/examples/jsm/geometries/TeapotGeometry'
 import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js'
+
+
+let scene, renderer, mesh;
+let cameraPersp, cameraOrtho, currentCamera;
+let textureCube;
+let spotLight, lightHelper, shadowCameraHelper;
+let control, orbit, points;
+let wireMaterial, pointMaterial, flatMaterial, gouraudMaterial, phongMaterial, texturedMaterial, reflectiveMaterial;
+let boxGeo, sphereGeo, teapotGeo, torusGeo, torusKoxGeo,cylinderGeo, coneGeo, tubeGeo ;
+
+
 /**
  * GUI Controls
  */
@@ -16,97 +27,124 @@ const gui = new dat.GUI()
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene()
+scene = new THREE.Scene()
 
-/**
- * Object
- */
-const geometry = new THREE.IcosahedronGeometry(20, 1)
-const material = new THREE.MeshNormalMaterial()
-// Material Props.
-material.wireframe = true
-// Create Mesh & Add To Scene
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+init()
 
-/**
- * Sizes
- */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+function init() {
+  renderer = new THREE.WebGLRenderer( { canvas: canvas,antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  renderer.shadowMap.enabled = true;
+
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.outputEncoding = THREE.sRGBEncoding;
 }
 
-window.addEventListener('resize', () => {
-  // Update sizes
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.001,
-  5000
-)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 50
-scene.add(camera)
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-controls.autoRotate = true
-// controls.enableZoom = false
-controls.enablePan = false
-controls.dampingFactor = 0.05
-controls.maxDistance = 1000
-controls.minDistance = 30
-controls.touches = {
-  ONE: THREE.TOUCH.ROTATE,
-  TWO: THREE.TOUCH.DOLLY_PAN,
-}
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  antialias: true,
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
-const clock = new THREE.Clock()
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
 
-  //mesh.rotation.y += 0.01 * Math.sin(1)
-  //mesh.rotation.y += 0.01 * Math.sin(1)
-  mesh.rotation.z += 0.01 * Math.sin(1)
 
-  // Update controls
-  controls.update()
-  // Render
-  renderer.render(scene, camera)
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
-}
 
-tick()
+
+
+
+
+// /**
+//  * Object
+//  */
+// const geometry = new THREE.IcosahedronGeometry(20, 1)
+// const material = new THREE.MeshNormalMaterial()
+// // Material Props.
+// material.wireframe = true
+// // Create Mesh & Add To Scene
+// mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+
+// /**
+//  * Sizes
+//  */
+// const sizes = {
+//   width: window.innerWidth,
+//   height: window.innerHeight,
+// }
+
+// window.addEventListener('resize', () => {
+//   // Update sizes
+//   sizes.width = window.innerWidth
+//   sizes.height = window.innerHeight
+
+//   // Update camera
+//   camera.aspect = sizes.width / sizes.height
+//   camera.updateProjectionMatrix()
+
+//   // Update renderer
+//   renderer.setSize(sizes.width, sizes.height)
+//   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// })
+
+// /**
+//  * Camera
+//  */
+// // Base camera
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   sizes.width / sizes.height,
+//   0.001,
+//   5000
+// )
+// camera.position.x = 1
+// camera.position.y = 1
+// camera.position.z = 50
+// scene.add(camera)
+
+// // Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+// controls.autoRotate = true
+// // controls.enableZoom = false
+// controls.enablePan = false
+// controls.dampingFactor = 0.05
+// controls.maxDistance = 1000
+// controls.minDistance = 30
+// controls.touches = {
+//   ONE: THREE.TOUCH.ROTATE,
+//   TWO: THREE.TOUCH.DOLLY_PAN,
+// }
+// /**
+//  * Renderer
+//  */
+// const renderer = new THREE.WebGLRenderer({
+//   canvas: canvas,
+//   antialias: true,
+// })
+// renderer.setSize(sizes.width, sizes.height)
+// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// /**
+//  * Animate
+//  */
+// const clock = new THREE.Clock()
+// const tick = () => {
+//   const elapsedTime = clock.getElapsedTime()
+
+//   //mesh.rotation.y += 0.01 * Math.sin(1)
+//   //mesh.rotation.y += 0.01 * Math.sin(1)
+//   mesh.rotation.z += 0.01 * Math.sin(1)
+
+//   // Update controls
+//   controls.update()
+//   // Render
+//   renderer.render(scene, camera)
+
+//   // Call tick again on the next frame
+//   window.requestAnimationFrame(tick)
+// }
+
+// tick()
